dc3d9a33d8eddc12d67a710bb8fcef5219d34c2e
Cody <rosecodyj@gmail.com>
2023-03-31 03:03:12 -0400

    baseline

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # StaTech-Industry
 Minecraft 1.19.2 modpack focused on Modern Industrializations
+
+
+This modpack includes Complimentary Reimagined Shaders: [https://github.com/ComplementaryDevelopment/ComplementaryReimagined](repo). Do not report any issues to the shader developers, but rather this mod's Discord.
---
+++ kubejs/server_scripts/script.js
@@ -0,0 +1,15 @@
+// priority: 0
+
+console.info('Hello, World! (You will see this line every time server resources reload)')
+
+ServerEvents.recipes(event => {
+	// Change recipes here
+})
+
+ServerEvents.tags('item', event => {
+	// Get the #forge:cobblestone tag collection and add Diamond Ore to it
+	// event.get('forge:cobblestone').add('minecraft:diamond_ore')
+
+	// Get the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
+	// event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
+})
