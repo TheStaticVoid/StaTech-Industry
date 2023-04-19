99aed4b126bfa376cff7517cbc09186c8718d809
Cody <staticthevoid@gmail.com>
2023-04-19 16:15:23 -0400

    Added Jon Bartman
    Closes #17

--- kubejs/startup_scripts/statech.js
+++ kubejs/startup_scripts/statech.js
@@ -5,4 +5,5 @@ StartupEvents.registry('item', e => {
     // -- DESH DRILL -- //
     e.create('space_probe').displayName('Space Probe');
     e.create('statech_logo').displayName('StaTech Logo');
+    e.create('bartman').displayName('Jon Bartman');
 });
