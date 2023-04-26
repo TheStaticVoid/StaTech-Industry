fc32f42526a35f5f36c60ba7548fdb8018c5874e
Cody <staticthevoid@gmail.com>
2023-04-26 18:49:06 -0400

    Add TR electrum to removed list

--- kubejs/server_scripts/loot_table.js
+++ kubejs/server_scripts/loot_table.js
@@ -17,7 +17,8 @@ LootJS.modifiers((event) => {
         tr('tungsten_ingot'),
         tr('aluminum_ingot'),
         tr('invar_ingot'),
-        tr('nickel_ingot')
+        tr('nickel_ingot'),
+		tr('electrum_ingot')
     ];
     TR_ITEMS.forEach(id => event.addLootTableModifier(/.*/).removeLoot(id));
 
