b04e1aab29434eacd1a7e672d29dc24972e3fc8d
Cody <staticthevoid@gmail.com>
2023-05-07 20:39:04 -0400

    KubeJS additions

--- kubejs/server_scripts/mods/timeinabottle.js
+++ kubejs/server_scripts/mods/timeinabottle.js
@@ -1,16 +1,49 @@
 ServerEvents.recipes(e => {
+    let st = (id) => `statech:${id}`;
+    let mi = (id) => `modern_industrialization:${id}`;
+    let mc = (id) => `minecraft:${id}`;
+    let sp = (id) => `spectrum:${id}`;
+
     e.remove({id: 'timeinabottle:time_in_a_bottle'});
-    e.shaped('timeinabottle:time_in_a_bottle', [
-        'GGG',
-        'DWD',
-        'LBL'
-    ],
-    {
-        G: 'modern_industrialization:gold_curved_plate',
-        D: 'minecraft:diamond',
-        W: 'minecraft:clock',
-        L: '#c:lapis',
-        B: 'minecraft:glass_bottle'
-    })
-    .id('statech:timeinabottle');
+    e.custom({
+        id: st('time_in_a_bottle'),
+        type: sp('pedestal'),
+        time: 400,
+        tier: 'simple',
+        cyan: 0,
+        magenta: 4,
+        yellow: 0,
+        white: 0,
+        black: 0,
+        experience: 5.0,
+        pattern: [
+            'GGG',
+            'DCD',
+            'ABA'
+        ],
+        key: {
+            G: {
+                item: mi('gold_curved_plate')
+            },
+            D: {
+                item: mc('diamond')
+            },
+            C: {
+                item: mc('clock')
+            },
+            A: {
+                item: sp('amethyst_powder')
+            },
+            B: {
+                item: mc('glass_bottle')
+            }
+        },
+        result: {
+            item: 'timeinabottle:time_in_a_bottle',
+            count: 1
+        },
+        required_advancement: [
+            sp('build_basic_pedestal_structure')
+        ]
+    });
 });
--- kubejs/server_scripts/mods/waystones.js
+++ kubejs/server_scripts/mods/waystones.js
@@ -1,70 +1,198 @@
 ServerEvents.recipes(e => {
     let st = (id) => `statech:waystones/${id}`;
+    let sp = (id) => `spectrum:${id}`;
+    let tr = (id) => `techreborn:${id}`;
+    let mc = (id) => `minecraft:${id}`;
+    let ws = (id) => `waystones:${id}`;
     const WAYSTONE_REMOVED_RECIPES = [
-        'waystones:warp_stone',
-        'waystones:return_scroll',
-        'waystones:bound_scroll',
-        'waystones:warp_scroll',
-        'waystones:warp_dust'
+        ws('warp_stone'),
+        ws('return_scroll'),
+        ws('bound_scroll'),
+        ws('warp_scroll'),
+        ws('warp_dust'),
+        ws('warp_plate')
     ];
     WAYSTONE_REMOVED_RECIPES.forEach(id => e.remove({id: id}));
 
     // -- WARP STONE -- //
-    e.shaped('waystones:warp_stone', [
-        'WPW',
-        'PCP',
-        'WPW'
-    ],
-    {
-        W: 'waystones:warp_dust',
-        P: '#c:ender_pearls',
-        C: 'modern_industrialization:electronic_circuit'
-    })
-    .id(st('warp_stone'));
+    e.custom({
+        id: st('warp_stone'),
+        type: sp('fusion_shrine'),
+        time: 400,
+        experience: 30.0,
+        ingredients: [
+            { item: ws('warp_dust'), count: 4 },
+            { item: mc('ender_pearl'), count: 4 },
+            { item: sp('onyx_shard') }
+        ],
+        result: {
+            item: ws('warp_stone')
+        },
+        required_advancement: sp('build_fusion_shrine'),
+        world_conditions: [],
+        start_crafting_effect: 'nothing',
+        during_crafting_effects: [
+            'visual_explosions_on_shrine',
+            'nothing',
+            'visual_explosions_on_shrine'
+        ],
+        finish_crafting_effect: 'lightning_on_shrine'
+    });
+
+    // -- WARP PLATE -- //
+    e.custom({
+        id: st('warp_plate'),
+        type: sp('pedestal'),
+        time: 400,
+        tier: 'simple',
+        cyan: 2,
+        magenta: 2,
+        yellow: 2,
+        white: 0,
+        black: 0,
+        experience: 10.0,
+        pattern: [
+            'SWS',
+            'WFW',
+            'SWS'
+        ],
+        key: {
+            W: { item: ws('warp_dust') },
+            F: { item: mc('flint') },
+            S: { item: mc('stone_bricks') }
+        },
+        result: {
+            item: ws('warp_plate'),
+            count: 2
+        },
+        required_advancement: [
+            sp('build_basic_pedestal_structure')
+        ]
+    });
 
     // -- RETURN SCROLL -- //
-    e.shaped('waystones:return_scroll', [
-        'GDG',
-        'PPP'
-    ],
-    {
-        G: '#c:gold_ingots',
-        D: 'waystones:warp_dust',
-        P: 'minecraft:paper'
-    })
-    .id(st('return_scroll'));
+    e.custom({
+        id: st('return_scroll'),
+        type: sp('pedestal'),
+        time: 200,
+        tier: 'simple',
+        cyan: 1,
+        magenta: 1,
+        yellow: 1,
+        white: 0,
+        black: 0,
+        experience: 10.0,
+        pattern: [
+            'GDG',
+            'PPP'
+        ],
+        key: {
+            G: { tag: 'c:gold_ingots' },
+            D: { item: ws('warp_dust') },
+            P: { item: mc('paper') }
+        },
+        result: {
+            item: ws('return_scroll'),
+            count: 1
+        },
+        required_advancement: [
+            sp('build_basic_pedestal_structure')
+        ]
+    });
 
     // -- BOUND SCROLL -- //
-    e.shaped('waystones:bound_scroll', [
-        'DDD',
-        'GEG',
-        'PPP'
-    ],
-    {
-        G: '#c:gold_ingots',
-        D: 'waystones:warp_dust',
-        P: 'minecraft:paper',
-        E: 'minecraft:ender_pearl'
-    })
-    .id(st('bound_scroll'));
+    e.custom({
+        id: st('bound_scroll'),
+        type: sp('pedestal'),
+        time: 200,
+        tier: 'simple',
+        cyan: 1,
+        magenta: 1,
+        yellow: 1,
+        white: 0,
+        black: 0,
+        experience: 10.0,
+        pattern: [
+            'DDD',
+            'GEG',
+            'PPP'
+        ],
+        key: {
+            D: { item: ws('warp_dust') },
+            G: { tag: 'c:gold_ingots' },
+            E: { item: mc('ender_pearl') },
+            P: { item: mc('paper') }
+        },
+        result: {
+            item: ws('bound_scroll'),
+            count: 1
+        },
+        required_advancement: [
+            sp('build_basic_pedestal_structure')
+        ]
+    });
 
     // -- WARP SCROLL -- //
-    e.shaped('waystones:warp_scroll', [
-        'GDG',
-        'GEG',
-        'PPP'
-    ],
-    {
-        G: '#c:gold_ingots',
-        D: 'waystones:warp_dust',
-        P: 'minecraft:paper',
-        E: 'minecraft:ender_pearl'
-    })
-    .id(st('warp_scroll'));
+    e.custom({
+        id: st('warp_scroll'),
+        type: sp('pedestal'),
+        time: 200,
+        tier: 'simple',
+        cyan: 1,
+        magenta: 1,
+        yellow: 1,
+        white: 0,
+        black: 0,
+        experience: 10.0,
+        pattern: [
+            'GDG',
+            'GEG',
+            'PPP'
+        ],
+        key: {
+            G: { tag: 'c:gold_ingots' },
+            D: { item: ws('warp_dust') },
+            P: { item: mc('paper') },
+            E: { item: mc('ender_pearl') }
+        },
+        result: {
+            item: ws('warp_scroll'),
+            count: 1
+        },
+        required_advancement: [
+            sp('build_basic_pedestal_structure')
+        ]
+    });
 
     // -- WARP DUST -- //
-    e.shapeless('4x waystones:warp_dust', [
-        '#c:ender_pearl_dusts', 'minecraft:amethyst_shard'
-    ])
-    .id(st('warp_dust'));
+    e.custom({
+        id: st('warp_dust'),
+        type: sp('pedestal'),
+        time: 200,
+        tier: 'simple',
+        cyan: 2,
+        magenta: 2,
+        yellow: 2,
+        white: 0,
+        black: 0,
+        experience: 10.0,
+        pattern: [
+            'EA'
+        ],
+        key: {
+            E: {
+                tag: 'c:ender_pearl_dusts'
+            },
+            A: {
+                item: sp('amethyst_powder')
+            }
+        },
+        result: {
+            item: ws('warp_dust'),
+            count: 4
+        },
+        required_advancement: [
+            sp('build_basic_pedestal_structure')
+        ]
+    });
 });
