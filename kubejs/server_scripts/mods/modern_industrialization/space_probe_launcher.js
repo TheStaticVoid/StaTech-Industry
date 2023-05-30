c293cb53cf72b15a703dfa5acc5782342b8a7131
Cody <staticthevoid@gmail.com>
2023-05-30 11:11:57 -0400

    Added more results to the space probe
    Closes #141

--- kubejs/server_scripts/mods/modern_industrialization/space_probe_launcher.js
+++ kubejs/server_scripts/mods/modern_industrialization/space_probe_launcher.js
@@ -24,13 +24,22 @@ ServerEvents.recipes(e => {
     spl(
         st('space_probe'),
         128,
-        12000,
+        6000,
         [ { amount: 1, item: kjs('space_probe') } ],
         [ 
-            { amount: 64, item: astra('deepslate_desh_ore') },
-            { amount: 64, item: astra('deepslate_ostrum_ore') },
-            { amount: 64, item: astra('deepslate_calorite_ore') },
-            { amount: 64, item: astra('deepslate_ice_shard_ore') }
+            { amount: 64, item: astra('moon_desh_ore') },
+            { amount: 64, item: astra('mars_ostrum_ore') },
+            { amount: 64, item: astra('venus_calorite_ore') },
+            { amount: 64, item: astra('deepslate_ice_shard_ore') },
+            { amount: 64, item: astra('moon_cheese_ore') },
+            { amount: 64, item: astra('moon_sand') },
+            { amount: 64, item: astra('moon_stone') },
+            { amount: 64, item: astra('mars_sand') },
+            { amount: 64, item: astra('mars_stone') },
+            { amount: 64, item: astra('venus_sand') },
+            { amount: 64, item: astra('venus_stone') },
+            { amount: 64, item: astra('mercury_stone') },
+            { amount: 64, item: astra('infernal_spire_block') }
         ]
     );
 });
