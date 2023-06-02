be4612b643fb47f3996b4925768f50a877e94dd2
Cody <staticthevoid@gmail.com>
2023-06-02 18:29:07 -0400

    Add StaTech Coins
    Should be balanced, might add different ores to it later. Closes #169

---
+++ kubejs/server_scripts/statech.js
@@ -0,0 +1,269 @@
+ServerEvents.recipes(e => {
+    let st = (id) => `statech:${id}`;
+    let mc = (id) => `minecraft:${id}`;
+    let mi = (id) => `modern_industrialization:${id}`;
+    let tr = (id) => `techreborn:${id}`;
+    let sp = (id) => `spectrum:${id}`;
+
+    let common = 'kubejs:coin_common';
+    let rare = 'kubejs:coin_rare';
+    let legendary = 'kubejs:coin_legendary';
+    let mythic = 'kubejs:coin_mythic';
+
+    e.shapeless(rare, [ '9x ' + common ]).id(st('coin_rare'));
+    e.shapeless(legendary, [ '9x ' + rare ]).id(st('coin_legendary'));
+    e.shapeless(mythic, [ '9x ' + legendary ]).id(st('coin_mythic'));
+
+    // -------------- //
+    //    COMMON      //
+    // -------------- //
+
+    // -- IRON ORE -- //
+    e.shaped('16x ' + mc('iron_ore'), [
+        'C C',
+        '   ',
+        'C C'
+    ],
+    {
+        C: common
+    }).id(st('iron_ore_from_common'));
+
+    // -- TIN ORE -- //
+    e.shaped('16x ' + mi('tin_ore'), [
+        'C C',
+        ' C ',
+        ' C '
+    ],
+    {
+        C: common
+    }).id(st('tin_ore_from_common'));
+
+    // -- RUBBER SAPLING -- //
+    e.shaped(tr('rubber_sapling'), [
+        'C',
+        'C'
+    ],
+    {
+        C: common
+    }).id(st('rubber_sapling_from_common'));
+
+    // -- ANDESITE -- //
+    e.shaped('32x ' + mc('andesite'), [
+        'C',
+        ' ',
+        'C'
+    ],
+    {
+        C: common
+    }).id(st('andesite_from_common'));
+
+    // -- AMETHYST SHARD -- //
+    e.shaped('16x ' + mc('amethyst_shard'), [
+        'C  ',
+        ' C ',
+        '  C'
+    ],
+    {
+        C: common
+    }).id('amethyst_shard_from_common');
+    
+    // -- TOPAZ SHARD -- //
+    e.shaped('16x ' + sp('topaz_shard'), [
+        '  C',
+        ' C ',
+        'C  '
+    ],
+    {
+        C: common
+    }).id(st('topaz_shard_from_common'));
+
+    // -- CITRINE SHARD -- //
+    e.shaped('16x ' + sp('citrine_shard'), [
+        'C  ',
+        'C  ',
+        'C  '
+    ],
+    {
+        C: common
+    }).id(st('citrine_shard_from_common'));
+
+    e.shaped('32x ' + mc('coal_ore'), [
+        'C C',
+        ' C ',
+        'C C'
+    ],
+    {
+        C: common
+    }).id(st('coal_ore_from_common'));
+
+    e.shaped('64x ' + mc('copper_ore'), [
+        'CCC',
+        'C  ',
+        'CCC'
+    ],
+    {
+        C: common
+    }).id(st('copper_ore_from_common'));
+
+    e.shaped('16x create:zinc_ore', [
+        'CC',
+        'CC'
+    ],
+    {
+        C: common
+    }).id(st('zinc_ore_from_common'));
+
+    // -- SALT ORE -- //
+    e.shaped('16x ' + mi('salt_ore'), [
+        ' CC',
+        'CC '
+    ],
+    {
+        C: common
+    }).id(st('salt_ore_from_common'));
+
+    // -------------- //
+    //      RARE      //
+    // -------------- //
+
+    // -- GOLD ORE -- //
+    e.shaped('32x ' + mc('gold_ore'), [
+        ' C ',
+        'C C',
+        ' C '
+    ],
+    {
+        C: rare
+    }).id(st('gold_ore_from_rare'));
+
+    // -- ANTIMONY ORE -- //
+    e.shaped('16x ' + mi('antimony_ore'), [
+        'C C',
+        '   ',
+        'C C'
+    ],
+    {
+        C: rare
+    }).id(st('antimony_ore_from_rare'));
+
+    // -- DIAMOND ORE -- //
+    e.shaped('8x ' + mc('diamond_ore'), [
+        'C',
+        'C'
+    ],
+    {
+        C: rare
+    }).id(st('diamond_ore_from_rare'));
+
+    // -- REDSTONE ORE -- //
+    e.shaped('16x ' + mc('redstone_ore'), [
+        'C C',
+        ' C ',
+        'C C'
+    ],
+    {
+        C: rare
+    }).id(st('redstone_ore_from_rare'));
+
+    // -- LEAD ORE -- //
+    e.shaped('16x ' + mi('lead_ore'), [
+        'CC',
+        'CC'
+    ],
+    {
+        C: rare
+    }).id(st('lead_ore_from_rare'));
+
+    // -- NICKEL ORE -- //
+    e.shaped('16x ' + mi('nickel_ore'), [
+        'CCC',
+        ' C '
+    ],
+    {
+        C: rare
+    }).id(st('nickel_ore_from_rare'));
+
+    // -- BAUXITE ORE -- //
+    e.shaped('16x ' + mi('bauxite_ore'), [
+        ' C ',
+        'CCC'
+    ],
+    {
+        C: rare
+    }).id(st('bauxite_ore_from_rare'));
+
+    // -------------- //
+    //   LEGENDARY    //
+    // -------------- //
+
+    // -- ANCIENT DEBRIS -- //
+    e.shaped('4x ' + mc('ancient_debris'), [
+        'C C',
+        '   ',
+        'C C'
+    ],
+    {
+        C: legendary
+    }).id(st('ancient_debris_from_legendary'));
+
+    // -- TUNGSTEN ORE -- //
+    e.shaped('16x ' + mi('tungsten_ore'), [
+        ' C ',
+        'C C',
+        ' C '
+    ],
+    {
+        C: legendary
+    }).id(st('tungsten_ore_from_legendary'));
+
+    // -- MOZANITE ORE -- //
+    e.shaped('16x ' + mi('mozanite_ore'), [
+        'CCC',
+        ' C '
+    ],
+    {
+        C: legendary
+    }).id(st('mozanite_ore_from_legendary'));
+
+    // -- URANIUM ORE -- //
+    e.shaped('8x ' + mi('uranium_ore'), [
+        'C C',
+        'C C',
+        'CCC'
+    ],
+    {
+        C: legendary
+    }).id(st('uranium_ore_from_legendary'));
+
+    // -------------- //
+    //     MYTHIC     //
+    // -------------- //
+
+    e.shaped(Item.of('minecraft:enchanted_book').enchant('minecraft:mending', 1), [
+        ' C ',
+        'C C',
+        ' C '
+    ],
+    {
+        C: mythic
+    }).id(st('mending_from_mythic'));
+
+    e.shaped(Item.of('minecraft:enchanted_book').enchant('yigd:soulbound', 1), [
+        'C C',
+        '   ',
+        'C C'
+    ],
+    {
+        C: mythic
+    }).id(st('soulboun_from_mythic'));
+});
+
+ServerEvents.tags('item', e => {
+    const COINS = [
+        'kubejs:coin_common',
+        'kubejs:coin_rare',
+        'kubejs:coin_legendary',
+        'kubejs:coin_mythic'
+    ];
+    COINS.forEach(id => e.add('kubejs:statech_coins', id));
+})
--- kubejs/startup_scripts/statech.js
+++ kubejs/startup_scripts/statech.js
@@ -2,5 +2,28 @@ StartupEvents.registry('item', e => {
     e.create('space_probe').displayName('Space Probe');
     e.create('statech_logo').displayName('StaTech Logo');
     e.create('bartman').displayName('Jon Bartman');
-    e.create('magic_diamond').displayName('Magic Diamond');
+    e.create('magic_diamond')
+        .displayName('Magic Diamond')
+        .rarity('Epic')
+        .tooltip('§dYour ticket to the Twilight Forest');
+
+    e.create('coin_common')
+        .displayName('Common StaTech Coin')
+        .rarity('Common')
+        .tooltip('§7Tier One Coin');
+
+    e.create('coin_rare')
+        .displayName('Rare StaTech Coin')
+        .rarity('Uncommon')
+        .tooltip('§aTier Two Coin');
+
+    e.create('coin_legendary')
+        .displayName('Legendary StaTech Coin')
+        .rarity('Rare')
+        .tooltip('§dTier Three Coin');
+
+    e.create('coin_mythic')
+        .displayName('Mythic StaTech Coin')
+        .rarity('Epic')
+        .tooltip('§cTier Four Coin');
 });
