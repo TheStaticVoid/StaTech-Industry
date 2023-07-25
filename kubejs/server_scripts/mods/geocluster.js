5ae599ae3d0bd7a56fb57b44aa4add3e9d15981f
static <TheStaticVoid@users.noreply.github.com>
2023-07-25 11:25:25 -0400

    Further Geocluster fixes
    #273

--- kubejs/server_scripts/mods/geocluster.js
+++ kubejs/server_scripts/mods/geocluster.js
@@ -3,27 +3,8 @@
 // STATECH INDUSTRY
 // -----------------------------------------
 
-ServerEvents.tags('item', e => {
+ServerEvents.recipes(e => {
     // -- MOD NAMESPACE UTILITY FUNCTIONS -- // 
     let gc = (id) => `geocluster:${id}`;
-
-    // ALUMINUM NOT INCLUDED BECAUSE HAHA FUNNY BRITISH SPELLING
-    // REMOVED MANUALLY FROM REI
-    const materials = [
-        'zinc',
-        'silver',
-        'lead',
-        'uranium',
-        'tin',
-        'platinum',
-        'titanium',
-        'nickel',
-    ];
-
-    // Tag all the materials
-    materials.forEach(id => {
-        e.add(`c:raw_${id}_ores`, gc(`raw_${id}`));
-        e.add(`c:${id}_ores`, gc(`${id}_ore`));
-        e.add(`c:${id}_ores`, gc(`deepslate_${id}_ore`));
-    });
+    e.remove({mod: 'geocluster', not: {id: gc('prospectors_pick') }});
 });
