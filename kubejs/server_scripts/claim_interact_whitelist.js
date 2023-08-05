b7f6e4f30c062141ee218daf5b5f26c5e8d8ddc4
redcatone <24564581+redcatone@users.noreply.github.com>
2023-08-04 20:39:21 -0400

    Use ServerEvents

--- kubejs/server_scripts/claim_interact_whitelist.js
+++ kubejs/server_scripts/claim_interact_whitelist.js
@@ -1,4 +1,4 @@
-onEvent('block.tags', (event) => {
-    const blocks = [/lootr:lootr_\w+/];
-    event.get('ftbchunks:interact_whitelist').add(blocks);
+ServerEvents.tags('block', (event) => {
+    const blocks = /lootr:lootr_\w+/;
+    event.add('ftbchunks:interact_whitelist', blocks);
 });
