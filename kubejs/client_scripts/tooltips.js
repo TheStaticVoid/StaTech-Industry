8e76d723d81002e6e0e046d4a78a17d0f14a6d45
Dmitry Proskurin <dmitriy-proskurin37@gmail.com>
2025-01-18 14:39:18 +0700

    Double tooltip fix

--- kubejs/client_scripts/tooltips.js
+++ kubejs/client_scripts/tooltips.js
@@ -29,7 +29,10 @@ ItemEvents.tooltip(e => {
     ];
 
     // -- ADD TOOLTIP TO ALL MACHINES IN LIST -- //
-    e.add(CUSTOM_MACHINES, Text.of('Added by StaTech Industry').gold());
-
-    e.add(kj('clear_glass'), Text.of('Completely see through when placed').yellow());
+    e.addAdvanced(CUSTOM_MACHINES, (item, adv, text) => {
+        text.add(Text.gold('Added by StaTech Industry'));
+	});
+    e.addAdvanced(kj('clear_glass'), (item, adv, text) => {
+        text.add(Text.yellow('Completely see through when placed'));
+    });
 });
