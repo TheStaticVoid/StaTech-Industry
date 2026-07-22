16f2a107f3868e518b1e13573c912c787fd98fbf
truepurpledino <75775263+truepurpledino@users.noreply.github.com>
2026-07-22 14:47:09 -0400

    remove unused/old folders. if any mishaps happen, they are on the fork repo

--- kubejs/disabled/server_scripts/mods/catwalks.js
+++
@@ -1,9 +0,0 @@
-// -----------------------------------------
-// CREATED BY STATIC FOR USE IN
-// STATECH INDUSTRY
-// -----------------------------------------
-
-ServerEvents.recipes(e => {
-    // Remove the iron rod from this mod
-    e.remove( {id: 'catwalksinc:iron_rod'} );
-});
