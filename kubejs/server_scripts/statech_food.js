ec49012c2f48018346202b6d8bfe4f1a38617481
fmbellomy <fmbellomy@quantumgarbage.com>
2026-07-26 22:49:47 -0500

    restore original formatting in statech_food

--- kubejs/server_scripts/statech_food.js
+++ kubejs/server_scripts/statech_food.js
@@ -4,16 +4,18 @@
 // -----------------------------------------
 
 ItemEvents.foodEaten(event => {
+    /* eslint-disable */
     const EatenOutputs = [
         // Eaten food                   // Item to give back
-        [kj('statech_energy'), kj('empty_can')],
-        [kj('bepsi'), kj('empty_can')],
-        [kj('coke_cola'), kj('empty_can')],
-        [kj('greg_cola'), kj('empty_can')],
-        [kj('sulfuric_acid_bottle'), mc('glass_bottle')],
-        [kj('uranium_cereal'), mc('bowl')],
-        [kj('nuka_cola'), kj('bottle_cap')]
+        [ kj('statech_energy'),         kj('empty_can') ],
+        [ kj('bepsi'),                  kj('empty_can') ],
+        [ kj('coke_cola'),              kj('empty_can') ],
+        [ kj('greg_cola'),              kj('empty_can') ],
+        [ kj('sulfuric_acid_bottle'),   mc('glass_bottle') ],
+        [ kj('uranium_cereal'),         mc('bowl') ],
+        [ kj('nuka_cola'),              kj('bottle_cap') ]
     ];
+    /* eslint-enable */
 
     EatenOutputs.forEach(eatenOutput => {
         if (event.getItem() == Item.of(eatenOutput[0])) {
