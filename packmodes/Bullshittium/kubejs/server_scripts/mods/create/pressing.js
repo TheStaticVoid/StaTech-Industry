f559e215ceb45e34ec6c413665011c7dc183f9f4
breck768 <breckj768@gmail.com>
2026-08-02 12:03:00 -0500

    thing

--- packmodes/Bullshittium/kubejs/server_scripts/mods/create/pressing.js
+++ packmodes/Bullshittium/kubejs/server_scripts/mods/create/pressing.js
@@ -3,8 +3,8 @@
 // STATECH INDUSTRY
 // -----------------------------------------
 
-ServerEvents.recipes(event => {
-    // -- MOD NAMESPACE UTILITY FUNCTIONS -- // 
+ServerEvents.recipes((event) => {
+    // -- MOD NAMESPACE UTILITY FUNCTIONS -- //
     let st = (id) => `statech:create/pressing/${id}`;
 
     // -- PRESSING REMOVED RECIPES -- //
@@ -14,7 +14,7 @@ ServerEvents.recipes(event => {
         cr('pressing/copper_ingot'),
         cr('pressing/gold_ingot'),
     ];
-    REMOVED_RECIPES.forEach(id => event.remove({ id: id }));
+    REMOVED_RECIPES.forEach((id) => event.remove({ id: id }));
 
     event.remove({ id: 'createaddition:pressing/electrum_ingot' }) //ok
 
@@ -22,42 +22,40 @@ ServerEvents.recipes(event => {
     let pressing = (id, item_inputs, item_outputs) => {
         let newRecipe = {
             type: cr('pressing'),
-        }
+        };
 
-        if (item_inputs)
-            newRecipe['ingredients'] = item_inputs;
-        if (item_outputs)
-            newRecipe['results'] = item_outputs;
+        if (item_inputs) newRecipe['ingredients'] = item_inputs;
+        if (item_outputs) newRecipe['results'] = item_outputs;
 
         event.custom(newRecipe).id(id);
-    }
+    };
 
     // -- BRONZE PLATE -- //
     pressing(
         st('bronze_plate'),
-        [ { tag: 'c:double_ingots/bronze' } ],
-        [ { 'id': mi('bronze_plate'), count: 1 } ]
+        [{ tag: 'c:double_ingots/bronze' }],
+        [{ 'id': mi('bronze_plate'), count: 1 }]
     );
 
     // -- SILVER PLATE -- //
     pressing(
         st('silver_plate'),
-        [{ tag: 'c:double_ingots/silver' } ],
-        [ { 'id': mi('silver_plate'), count: 1 } ]
+        [{ tag: 'c:double_ingots/silver' }],
+        [{ 'id': mi('silver_plate'), count: 1 }]
     );
 
     // -- STEEL PLATE -- //
     pressing(
         st('steel_plate'),
-        [ { tag: 'c:double_ingots/steel' } ],
-        [ { 'id': mi('steel_plate'), count: 1 } ]
+        [{ tag: 'c:double_ingots/steel' }],
+        [{ 'id': mi('steel_plate'), count: 1 }]
     );
 
     // -- TIN PLATE -- //
     pressing(
         st('tin_plate'),
-        [{ tag: 'c:double_ingots/tin' } ],
-        [ { 'id': mi('tin_plate'), count: 1 } ]
+        [{ tag: 'c:double_ingots/tin' }],
+        [{ 'id': mi('tin_plate'), count: 1 }]
     );
 
     // -- BULLSHITTIUM MODE -- //
