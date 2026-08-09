e58f650abdd9296eef563a0ae48c6f7f2dbef2a2
truepurpledino <75775263+truepurpledino@users.noreply.github.com>
2026-08-09 18:29:51 -0400

    reclycer changes

--- kubejs/server_scripts/mods/modern_industrialization/recycler.js
+++ kubejs/server_scripts/mods/modern_industrialization/recycler.js
@@ -17,7 +17,7 @@ ServerEvents.recipes((event) => {
         [{ amount: 1, item: kj('scrap'), probability: 0.35 }]
     );
 
-    // -- LOW-MID TIER SCRAP -- //
+    // -- MID TIER SCRAP -- //
     recycler(
         event,
         st('mid_tier_scrap'),
@@ -25,16 +25,6 @@ ServerEvents.recipes((event) => {
         50,
         [{ amount: 1, tag: 'statech:scrappable/mid' }],
         [{ amount: 2, item: kj('scrap'), probability: 0.5 }]
-    );
-
-    // -- HIGH-MID TIER SCRAP -- //
-    recycler(
-        event,
-        st('high_mid_tier_scrap'),
-        48,
-        200,
-        [{ amount: 1, tag: 'statech:scrappable/high_mid' }],
-        [{ amount: 1, item: kj('scrap'), probability: 0.5 }]
     );
 
     // -- HIGH TIER SCRAP -- //
@@ -283,8 +273,8 @@ ServerEvents.tags('item', (event) => {
         '#c:ores/fluorite',
         '#c:ores/diamond',
     ];
-    HIGH_MID_TIER_SCRAP.forEach((id) => {
-        event.add(st('high_mid'), id);
+    MID_TIER_SCRAP.forEach((id) => {
+        event.add(st('mid'), id);
     });
 
     let HIGH_TIER_SCRAP = [
