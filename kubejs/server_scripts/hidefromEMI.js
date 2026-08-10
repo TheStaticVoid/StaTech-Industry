eb184f044848afda84273344ee148e3f55c56c7c
gronk <grantewilliams51@gmail.com>
2026-08-10 15:42:41 -0500

    kill point blank and update mods

--- kubejs/server_scripts/hidefromEMI.js
+++ kubejs/server_scripts/hidefromEMI.js
@@ -133,9 +133,6 @@ RecipeViewerEvents.removeEntriesCompletely('item', (event) => {
         /^modern_industrialization:.*(cobalt).*/,
     ];
     UNUSED_DEV.forEach((id) => event.remove(id));
-
-    const UNUSED_PB = [pb('printer')];
-    UNUSED_PB.forEach((id) => event.remove(id));
 });
 
 RecipeViewerEvents.removeEntriesCompletely('fluid', (event) => {
