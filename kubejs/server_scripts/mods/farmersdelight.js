102be85325ff3038d4741ea956a7e775ab67e583
gronk <grantewilliams51@gmail.com>
2026-08-12 13:56:12 -0500

    Fix #674

--- kubejs/server_scripts/mods/farmersdelight.js
+++ kubejs/server_scripts/mods/farmersdelight.js
@@ -27,7 +27,21 @@ ServerEvents.recipes((event) => {
     FARMERSDELIGHT_REMOVED_RECIPES.forEach((id) => event.remove({ id: id }));
 });
 
-// -- MILK TAG UNIFICATION -- //
 ServerEvents.tags('item', (event) => {
+    // -- MILK TAG UNIFICATION -- //
     event.add('c:foods/milk', '#c:drinks/milk');
+
+    // -- CULTURAL DELIGHTS TAG FOODS -- //
+    event.add('c:foods', /^culturaldelights:.*/);
+    event.remove(
+        'c:foods',
+        /^culturaldelights:.*(seeds|kernels|wild|crate|leaves|wood|log|pit|sapling).*/
+    );
+
+    // -- CULTURAL DELIGHTS FEAST TAGGING -- //
+    event.add(fd('feasts'), cud('exotic_roll_medley'));
+    event.add('c:foods/edible_when_placed', cud('exotic_roll_medley'));
+
+    event.add(fd('feasts'), cud('eggplant_parmesan_block'));
+    event.add('c:foods/edible_when_placed', cud('eggplant_parmesan_block'));
 });
