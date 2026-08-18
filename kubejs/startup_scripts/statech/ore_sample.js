248c655c7ba77726ec1af3861f92dad45349a463
static <TheStaticVoid@users.noreply.github.com>
2026-08-18 12:12:34 -0400

    update sample to match original mod

--- kubejs/startup_scripts/statech/ore_sample.js
+++ kubejs/startup_scripts/statech/ore_sample.js
@@ -11,9 +11,11 @@ StartupEvents.registry('block', (event) => {
 let createSample = (event, id, name) => {
     event.create(id)
         .displayName(name)
-        .soundType('stone')
+        .soundType('tuff')
+        .hardness(0.1)
         .fullBlock(false)
         .waterlogged()
         .notSolid()
+        .opaque(false)
         .box(1.0, 0.0, 1.0, 13.0, 3.0, 13.0);
 };
