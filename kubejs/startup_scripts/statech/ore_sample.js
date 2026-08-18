cb8ae07c4dffd414ec19d5cf664e8ab242cf22d5
static <TheStaticVoid@users.noreply.github.com>
2026-08-18 12:05:37 -0400

    uranium sample + fix titanium

--- kubejs/startup_scripts/statech/ore_sample.js
+++ kubejs/startup_scripts/statech/ore_sample.js
@@ -3,11 +3,17 @@
 // STATECH INDUSTRY 2
 // -----------------------------------------
 StartupEvents.registry('block', (event) => {
-    event.create('titanium_ore_sample')
-        .displayName('Titanium Ore Sample')
+    createSample(event, 'titanium_ore_sample', 'Titanium Ore Sample');
+    createSample(event, 'uranium_ore_sample', 'Uranium Ore Sample');
+});
+
+
+let createSample = (event, id, name) => {
+    event.create(id)
+        .displayName(name)
         .soundType('stone')
         .fullBlock(false)
         .waterlogged()
         .notSolid()
         .box(1.0, 0.0, 1.0, 13.0, 3.0, 13.0);
-});
+};
