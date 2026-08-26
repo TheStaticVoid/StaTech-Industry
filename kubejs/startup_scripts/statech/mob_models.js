d2b15248edeafb3f993cb52f93a598d11252a968
static <TheStaticVoid@users.noreply.github.com>
2026-08-26 12:23:55 -0400

    Shorten mod model / ore sample scripts

--- kubejs/startup_scripts/statech/mob_models.js
+++ kubejs/startup_scripts/statech/mob_models.js
@@ -4,167 +4,45 @@
 // -----------------------------------------
 
 StartupEvents.registry('item', (event) => {
-    event
-        .create('enderman_model')
-        .displayName('Enderman Model')
-        .rarity('Rare')
-        .tag('kubejs:mob_models')
-        .tooltip('§3Designates the output in the Mob Crusher');
+    let mobModel = (name) => {
+        event
+            .create(`${name}_model`)
+            .rarity('Rare')
+            .tag('kubejs:mob_models')
+            .tooltip('§3Designates the output in the Mob Crusher');
+    };
 
-    event
-        .create('zombie_model')
-        .displayName('Zombie Model')
-        .rarity('Rare')
-        .tag('kubejs:mob_models')
-        .tooltip('§3Designates the output in the Mob Crusher');
+    let bossModel = (name) => {
+        event
+            .create(`${name}_model`)
+            .rarity('Epic')
+            .tag('kubejs:mob_models')
+            .tag('kubejs:boss_models')
+            .tooltip('§3Designates the output in the Boss Crusher');
+    }
 
-    event
-        .create('skeleton_model')
-        .displayName('Skeleton Model')
-        .rarity('Rare')
-        .tag('kubejs:mob_models')
-        .tooltip('§3Designates the output in the Mob Crusher');
+    mobModel('enderman');
+    mobModel('zombie');
+    mobModel('skeleton');
+    mobModel('pig');
+    mobModel('sheep');
+    mobModel('cow');
+    mobModel('chicken');
+    mobModel('squid');
+    mobModel('spider');
+    mobModel('creeper');
+    mobModel('slime');
+    mobModel('ghast');
+    mobModel('zombie_piglin');
+    mobModel('magma_cube');
+    mobModel('blaze');
+    mobModel('wither_skeleton');
+    mobModel('guardian');
+    mobModel('witch');
+    mobModel('rabbit');
+    mobModel('phantom');
 
-    event
-        .create('pig_model')
-        .displayName('Pig Model')
-        .rarity('Rare')
-        .tag('kubejs:mob_models')
-        .tooltip('§3Designates the output in the Mob Crusher');
-
-    event
-        .create('sheep_model')
-        .displayName('Sheep Model')
-        .rarity('Rare')
-        .tag('kubejs:mob_models')
-        .tooltip('§3Designates the output in the Mob Crusher');
-
-    event
-        .create('cow_model')
-        .displayName('Cow Model')
-        .rarity('Rare')
-        .tag('kubejs:mob_models')
-        .tooltip('§3Designates the output in the Mob Crusher');
-
-    event
-        .create('chicken_model')
-        .displayName('Chicken Model')
-        .rarity('Rare')
-        .tag('kubejs:mob_models')
-        .tooltip('§3Designates the output in the Mob Crusher');
-
-    event
-        .create('squid_model')
-        .displayName('Squid Model')
-        .rarity('Rare')
-        .tag('kubejs:mob_models')
-        .tooltip('§3Designates the output in the Mob Crusher');
-
-    event
-        .create('spider_model')
-        .displayName('Spider Model')
-        .rarity('Rare')
-        .tag('kubejs:mob_models')
-        .tooltip('§3Designates the output in the Mob Crusher');
-
-    event
-        .create('creeper_model')
-        .displayName('Creeper Model')
-        .rarity('Rare')
-        .tag('kubejs:mob_models')
-        .tooltip('§3Designates the output in the Mob Crusher');
-
-    event
-        .create('slime_model')
-        .displayName('Slime Model')
-        .rarity('Rare')
-        .tag('kubejs:mob_models')
-        .tooltip('§3Designates the output in the Mob Crusher');
-
-    event
-        .create('ghast_model')
-        .displayName('Ghast Model')
-        .rarity('Rare')
-        .tag('kubejs:mob_models')
-        .tooltip('§3Designates the output in the Mob Crusher');
-
-    event
-        .create('zombie_piglin_model')
-        .displayName('Zombie Piglin Model')
-        .rarity('Rare')
-        .tag('kubejs:mob_models')
-        .tooltip('§3Designates the output in the Mob Crusher');
-
-    event
-        .create('magma_cube_model')
-        .displayName('Magma Cube Model')
-        .rarity('Rare')
-        .tag('kubejs:mob_models')
-        .tooltip('§3Designates the output in the Mob Crusher');
-
-    event
-        .create('blaze_model')
-        .displayName('Blaze Model')
-        .rarity('Rare')
-        .tag('kubejs:mob_models')
-        .tooltip('§3Designates the output in the Mob Crusher');
-
-    event
-        .create('wither_skeleton_model')
-        .displayName('Wither Skeleton Model')
-        .rarity('Rare')
-        .tag('kubejs:mob_models')
-        .tooltip('§3Designates the output in the Mob Crusher');
-
-    event
-        .create('guardian_model')
-        .displayName('Guardian Model')
-        .rarity('Rare')
-        .tag('kubejs:mob_models')
-        .tooltip('§3Designates the output in the Mob Crusher');
-
-    event
-        .create('witch_model')
-        .displayName('Witch Model')
-        .rarity('Rare')
-        .tag('kubejs:mob_models')
-        .tooltip('§3Designates the output in the Mob Crusher');
-
-    event
-        .create('rabbit_model')
-        .displayName('Rabbit Model')
-        .rarity('Rare')
-        .tag('kubejs:mob_models')
-        .tooltip('§3Designates the output in the Mob Crusher');
-
-    event
-        .create('phantom_model')
-        .displayName('Phantom Model')
-        .rarity('Rare')
-        .tag('kubejs:mob_models')
-        .tooltip('§3Designates the output in the Mob Crusher');
-
-    event
-        .create('wither_model')
-        .displayName('Wither Model')
-        .rarity('Epic')
-        .tag('kubejs:mob_models')
-        .tag('kubejs:boss_models')
-        .tooltip('§3Designates the output in the Boss Crusher');
-
-    event
-        .create('dragon_model')
-        .displayName('Dragon Model')
-        .rarity('Epic')
-        .tag('kubejs:mob_models')
-        .tag('kubejs:boss_models')
-        .tooltip('§3Designates the output in the Boss Crusher');
-
-    event
-        .create('warden_model')
-        .displayName('Warden Model')
-        .rarity('Epic')
-        .tag('kubejs:mob_models')
-        .tag('kubejs:boss_models')
-        .tooltip('§3Designates the output in the Boss Crusher');
+    bossModel('wither');
+    bossModel('dragon');
+    bossModel('warden');
 });
